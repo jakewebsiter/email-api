7d70e52e05341a40348263acf12ac2fc8fdb717f
Jake Harris <jakeharris30@outlook.com>
2024-05-11 10:14:05 +0100

    added requirements.txt & domain

--- email_api_project/settings.py
+++ email_api_project/settings.py
@@ -48,9 +48,7 @@ INSTALLED_APPS = [
 ]
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500",
-]
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:5500", "http://manyemails-2.onrender.com"]
 
 
 MIDDLEWARE = [
